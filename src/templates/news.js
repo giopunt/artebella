c4f236361429267c9227210d21e746674d33ba75
Giovanni Puntil <puntilgiovanni@gmail.com>
2020-05-13 20:25:56 +0100

    Update basic news layout

--- src/templates/news.js
+++ src/templates/news.js
@@ -1,16 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import styles from "./news.module.css"
+
 import Layout from "../components/layout"
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div>
+      <div className={styles.content}>
       news —
         <h1>{post.frontmatter.title}</h1>
-        <img src={post.frontmatter.hero} />
+        <img className={styles.hero} src={post.frontmatter.hero} />
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
